b295e71baff19c923bc26fcfb3b7ad4fb20ff368
INT3NSE07 <d.jonathanpaul@gmail.com>
2017-06-02 10:24:38 +0530

    sched: Fix EAS mismerge

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1175,7 +1175,6 @@ static const u32 prio_to_wmult[40] = {
 #define DEQUEUE_SLEEP		0x01
 #define DEQUEUE_SAVE		0x02 /* matches ENQUEUE_RESTORE */
 #define DEQUEUE_MOVE		0x04 /* matches ENQUEUE_MOVE */
-#define DEQUEUE_MIGRATING	0x08
 
 #define ENQUEUE_WAKEUP		0x01
 #define ENQUEUE_RESTORE		0x02
@@ -1188,11 +1187,7 @@ static const u32 prio_to_wmult[40] = {
 #else
 #define ENQUEUE_WAKING		0x00
 #endif
-#define ENQUEUE_REPLENISH	0x08
-#define ENQUEUE_RESTORE	0x10
 #define ENQUEUE_WAKEUP_NEW	0x20
-
-#define DEQUEUE_SLEEP		1
 
 #define RETRY_TASK		((void *)-1UL)
 
